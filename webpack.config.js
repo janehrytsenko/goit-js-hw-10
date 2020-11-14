c50fe0be0db1495def7c808008a941839d80cf4f
Yevheniia Hrytsenko <yevheniiahrytsenko@gmail.com>
2020-11-14 14:12:13 +0200

    Webpack DevServer added

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      }
+    ],
   },
 };
